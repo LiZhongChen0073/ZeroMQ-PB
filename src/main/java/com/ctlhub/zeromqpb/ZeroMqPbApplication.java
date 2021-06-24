baf1285ce105957e3d0849bd7d62b385e06654fd
李重辰 <lizhongchen@ones.ai>
2021-06-24 23:42:52 +0800

    HelloWordZMQ

---
+++ src/main/java/com/ctlhub/zeromqpb/ZeroMqPbApplication.java
@@ -0,0 +1,13 @@
+package com.ctlhub.zeromqpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroMqPbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZeroMqPbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ctlhub/zeromqpb/ZeroMqPbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctlhub.zeromqpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroMqPbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
