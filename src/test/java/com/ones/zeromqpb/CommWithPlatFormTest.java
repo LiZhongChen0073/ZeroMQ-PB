1d0f0d27d499448bec2cad9c72fee02fbea7656c
李重辰 <lizhongchen@ones.ai>
2021-06-28 22:30:14 +0800

    Update CommWithPlatFormTest.java

--- src/test/java/com/ones/zeromqpb/CommWithPlatFormTest.java
+++ src/test/java/com/ones/zeromqpb/CommWithPlatFormTest.java
@@ -1,7 +1,10 @@
 package com.ones.zeromqpb;
 
+import com.ones.openplatform.protocol.Common;
 import com.ones.openplatform.protocol.Message;
 import org.junit.Test;
+import org.zeromq.SocketType;
+import org.zeromq.ZSocket;
 
 /**
  * 与开放平台通信
@@ -10,6 +13,18 @@ public class CommWithPlatFormTest {
 
   @Test
   public void sayHello() {
-//    final Message.PlatformMessage.Builder builder = Message.PlatformMessage.newBuilder().setCmdb();
+    Common.RouterNode.Builder routerBuilder = Common.RouterNode.newBuilder();
+    routerBuilder.setID("H0000001");
+    Common.RouterMessage.Builder routerMessageBuilder = Common.RouterMessage.newBuilder();
+    routerMessageBuilder.setDistinct(routerBuilder.build());
+    Message.PlatformMessage.Builder platformMessageBuilder = Message.PlatformMessage.newBuilder();
+    platformMessageBuilder.setHeader(routerMessageBuilder.build());
+    System.out.println(platformMessageBuilder.toString());
+
+    try(ZSocket socket = new ZSocket(SocketType.DEALER);){
+      socket.connect("tcp://127.0.0.1:12345");
+      socket.send(platformMessageBuilder.build().toByteArray(),1);
+    }
+
   }
 }
